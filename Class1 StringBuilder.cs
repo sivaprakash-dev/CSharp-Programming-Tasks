967e108041907d220edda2cccdda1dc90da577ef
Sivaprakash D <sivaprakash09042005@gmail.com>
2026-06-25 10:38:24 +0530

    Add files via upload

---
+++ Class1 StringBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace string___StringBuilder
+{
+    internal class Class1_StringBuilder
+    {
+        public static void Main(string[] args)
+        {
+            StringBuilder name = new StringBuilder("Wellcome To All");
+
+            //Append
+            name.Append("Hello");
+            Console.WriteLine(name);
+
+            //AppendLine
+            name.AppendLine("Siva");
+            Console.WriteLine(name);
+
+
+            //Insert
+            name.Insert(12, "I am");
+            Console.WriteLine(name);
+
+
+            //Remove
+            StringBuilder empname = new StringBuilder("Hi Iam Siva");
+
+            empname.Remove(1,4);
+            Console.WriteLine(empname);
+
+
+            //Replace
+            empname.Replace("Siva","Prakash");
+            Console.WriteLine(empname);
+
+
+            //Clear
+            empname.Clear();
+            Console.WriteLine(empname);
+
+
+
+            //ToString
+            StringBuilder Detail = new StringBuilder("CHENNAI");
+
+            string d = Detail.ToString();
+            Console.WriteLine(d);
+        }
+    }
+}
---
+++ String type.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string___StringBuilder
+{
+    internal class Program
+    {
+        // String is Immutable
+
+
+        static void Main(string[] args)
+        {
+            //Length
+
+            string name = "Sivaprakash";
+            int length = name.Length;
+            Console.WriteLine(length);
+
+            //ToUpper
+
+            name = name.ToUpper();
+            Console.WriteLine(name);
+
+            //ToLower
+
+            name = name.ToLower();
+            Console.WriteLine(name);
+
+
+            //Substring
+
+            name = name.Substring(4);
+            Console.WriteLine(name);
+
+
+            //Contains
+
+            string empname = "Pravinkumar";
+            bool res = empname.Contains("Pravin");
+            Console.WriteLine(res);
+
+
+            //Replace
+
+            string na = empname.Replace("Pravin", "Siva");
+            Console.WriteLine(na);
+
+
+            //Trim
+
+            string va = empname.Trim();
+            Console.WriteLine(va);
+
+
+
+            //IndexOf
+
+            int po = empname.IndexOf("i");
+            Console.WriteLine(po);
+        }
+    }
+}
